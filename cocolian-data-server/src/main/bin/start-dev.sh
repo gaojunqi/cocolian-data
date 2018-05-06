238fb5fbd988527787272b54f060d3c388ab8a97
Shamphone Lee <shamphone@gmail.com>
2018-05-06 17:00:08 +0100

    fix bash file errors

--- cocolian-data-server/src/main/bin/start-dev.sh
+++ cocolian-data-server/src/main/bin/start-dev.sh
@@ -1,14 +1,15 @@
- #!/bin/bash
- #
- # Author: Shamphone Lee
+#!/bin/bash
+#
+# Author: Shamphone Lee
 
+JAVA="$(cygpath -pm $JAVA_HOME/bin/java)"
 WORKING_DIR=$(cd `dirname $0`; pwd)
  
 WORKING_DIR="$(dirname ${WORKING_DIR})"
 
 if [ "$(expr substr $(uname -s) 1 9)" == "CYGWIN_NT" ]; then
 	echo "Cygwin detected"
-	WORKING_DIR="$(cygpath -m ${WORKING_DIR})"  
+#	WORKING_DIR="$(cygpath -m ${WORKING_DIR})"  
 fi
 
 echo "running in ${WORKING_DIR}"
@@ -16,19 +17,23 @@ echo "running in ${WORKING_DIR}"
 
 MAIN_CLASS="org.cocolian.data.server.DataServer"
 
-JAR_LIB=$WORKING_DIR/${project.build.finalName}.jar
-JAR_LIB="${JAR_LIB};${WORKING_DIR}/lib/*"
+JAR_LIB="$WORKING_DIR/${project.build.finalName}.jar"
+JAR_LIB="${JAR_LIB}:${WORKING_DIR}/lib/*"
+JAR_LIB="$(cygpath -pm ${JAR_LIB})"
+
+LOG_PATH="${WORKING_DIR}/logs"
+LOG_PATH="$(cygpath -pm ${LOG_PATH})"
 
 # JAVA_JVM_OPTION="-Xmx25g -Xms25g -XX:NewSize=15g -XX:MaxNewSize=15g -XX:SurvivorRatio=17 -XX:PermSize=256m -XX:MaxPermSize=256m"
 
 JAVA_JVM_OPTION="-Xmx1g -Xms1g"
-JAVA_JVM_OPTION="${JAVA_JVM_OPTION} -Duser.timezone=\"GMT+8\" -Dcocolian.log.path=\"${WORKING_DIR}/logs\""
+JAVA_JVM_OPTION="${JAVA_JVM_OPTION} -Duser.timezone=GMT+8 -Dcocolian.log.path=${LOG_PATH}"
 
 
-JAVA_JVM_OPTION="${JAVA_JVM_OPTION} -cp \"${JAR_LIB}\" ${MAIN_CLASS}  "
+JAVA_JVM_OPTION="${JAVA_JVM_OPTION} -cp ${JAR_LIB} ${MAIN_CLASS}  "
 
-echo "java  ${JAVA_JVM_OPTION}   2>&1 < /dev/null"
+echo "${JAVA}  ${JAVA_JVM_OPTION} "
 
-java ${JAVA_JVM_OPTION}  2>&1 < /dev/null
+${JAVA} ${JAVA_JVM_OPTION} 
     
       
